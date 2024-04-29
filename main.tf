e5bdc17a86bb4c451719b0b08a3ebb831eba31be
shipperizer <alexcabb@gmail.com>
2024-04-29 09:26:15 +0100

    feat: expose kratos-info as juju offer for potential oathkeeper integration

--- main.tf
+++ main.tf
@@ -4,3 +4,10 @@ resource "juju_offer" "oauth_offer" {
   application_name = juju_application.hydra.name
   endpoint         = "oauth"
 }
+
+resource "juju_offer" "kratos_info_offer" {
+  name             = "kratos-info-offer"
+  model            = var.model
+  application_name = juju_application.kratos.name
+  endpoint         = "kratos-info"
+}
