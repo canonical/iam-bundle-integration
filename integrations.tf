e95946cd34571878a15192a574f2dcebc31e0059
shipperizer <alexcabb@gmail.com>
2024-06-12 18:00:42 +0100

    fix: drop usage of data.juju_offer due to how it creates map

--- integrations.tf
+++ integrations.tf
@@ -59,29 +59,29 @@ locals {
       }
     ]
   ])
-  cmi_integration_mapping = [
-    {
-      offer_url = data.juju_offer.external_ingress.url
-      requirers = [
-        juju_application.kratos.name, juju_application.hydra.name
-      ]
-      requirer_endpoint = "public-ingress"
-    },
-    {
-      offer_url         = data.juju_offer.external_ingress.url
-      requirers         = [juju_application.login_ui.name]
-      requirer_endpoint = "ingress"
-    },
-  ]
-  cmi_integrations = flatten([
-    for mapping in local.cmi_integration_mapping : [
-      for requirer in toset(mapping.requirers) : {
-        offer_url         = mapping.offer_url
-        requirer          = requirer
-        requirer_endpoint = mapping.requirer_endpoint
-      }
-    ]
-  ])
+  // cmi_integration_mapping = [
+  //   {
+  //     offer_url = var.juju_offers.external_ingress_offer
+  //     requirers = [
+  //       juju_application.kratos.name, juju_application.hydra.name
+  //     ]
+  //     requirer_endpoint = "public-ingress"
+  //   },
+  //   {
+  //     offer_url         = var.juju_offers.external_ingress_offer
+  //     requirers         = [juju_application.login_ui.name]
+  //     requirer_endpoint = "ingress"
+  //   },
+  // ]
+  // cmi_integrations = flatten([
+  //   for mapping in local.cmi_integration_mapping : [
+  //     for requirer in toset(mapping.requirers) : {
+  //       offer_url         = mapping.offer_url
+  //       requirer          = requirer
+  //       requirer_endpoint = mapping.requirer_endpoint
+  //     }
+  //   ]
+  // ])
 }
 
 resource "juju_integration" "integration" {
@@ -107,23 +107,62 @@ resource "juju_integration" "integration" {
   }
 }
 
-resource "juju_integration" "cmi_integration" {
-  for_each = {
-    for idx, integration in local.cmi_integrations :
-    join(",", [
-      integration.offer_url,
-      join(":", [integration.requirer, integration.requirer_endpoint])
-    ]) => integration
-  }
+// resource "juju_integration" "cmi_integration" {
+//   for_each = {
+//     for idx, integration in local.cmi_integrations :
+//     join(",", [
+//       integration.offer_url,
+//       join(":", [integration.requirer, integration.requirer_endpoint])
+//     ]) => integration
+//   }
 
+//   model = var.model
+
+//   application {
+//     offer_url = each.value.offer_url
+//   }
+
+//   application {
+//     name     = each.value.requirer
+//     endpoint = each.value.requirer_endpoint
+//   }
+// }
+
+resource "juju_integration" "login_ui_public_ingress" {
   model = var.model
 
   application {
-    offer_url = each.value.offer_url
+    offer_url = data.juju_offer.external_ingress.url
   }
 
   application {
-    name     = each.value.requirer
-    endpoint = each.value.requirer_endpoint
+    name     = juju_application.login_ui.name
+    endpoint = "ingress"
+  }
+}
+
+resource "juju_integration" "hydra_public_ingress" {
+  model = var.model
+
+  application {
+    offer_url = data.juju_offer.external_ingress.url
+  }
+
+  application {
+    name     = juju_application.hydra.name
+    endpoint = "public-ingress"
+  }
+}
+
+resource "juju_integration" "kratos_public_ingress" {
+  model = var.model
+
+  application {
+    offer_url = data.juju_offer.external_ingress.url
+  }
+
+  application {
+    name     = juju_application.kratos.name
+    endpoint = "public-ingress"
   }
 }
