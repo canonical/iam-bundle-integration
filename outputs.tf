8cfca7ff86849643e44c9cad5d99b21cf2a1428f
shipperizer <alexcabb@gmail.com>
2024-08-15 10:27:36 +0200

    fix: add kratos and hydra app names as outputs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "oauth_offer_url" {
 output "kratos_info_offer_url" {
   value = juju_offer.kratos_info_offer.url
 }
+
+output "kratos" {
+  value = juju_application.kratos.name
+}
+
+output "hydra" {
+  value = juju_application.hydra.name
+}
