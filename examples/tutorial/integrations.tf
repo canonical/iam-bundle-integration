ecb547f6f8d730253d0cc8c1b66214b1e42a44a8
shipperizer <alexcabb@gmail.com>
2025-11-10 14:12:08 +0000

    fix: update example tutorial

--- examples/tutorial/integrations.tf
+++ examples/tutorial/integrations.tf
@@ -1,41 +1,40 @@
 resource "juju_offer" "ingress_offer" {
-  model            = juju_model.core.name
   name             = "ingress"
   application_name = module.traefik.app_name
   endpoints        = ["ingress"]
+  model_uuid       = juju_model.core.uuid
 }
 
 resource "juju_offer" "traefik_route_offer" {
-  model            = juju_model.core.name
   name             = "traefik-route"
   application_name = module.traefik.app_name
   endpoints        = ["traefik-route"]
+  model_uuid       = juju_model.core.uuid
 }
 
 resource "juju_offer" "postgresql_offer" {
-  model            = juju_model.core.name
   name             = "postgresql"
   application_name = module.postgresql.application_name
   endpoints        = ["database"]
+  model_uuid       = juju_model.core.uuid
 }
 
 resource "juju_offer" "send_ca_certificate_offer" {
-  model            = juju_model.core.name
   name             = "send-ca-cert"
   application_name = module.certificates.app_name
   endpoints        = ["send-ca-cert"]
+  model_uuid       = juju_model.core.uuid
 }
 
 resource "juju_offer" "openfga_offer" {
-  model            = juju_model.core.name
   count            = var.enable_admin_ui ? 1 : 0
   name             = "openfga"
-  application_name = juju_application.openfga[0].name
+  application_name = module.openfga.app_name
   endpoints        = ["openfga"]
+  model_uuid       = juju_model.core.uuid
 }
 
 resource "juju_integration" "traefik_certs" {
-  model = juju_model.core.name
   application {
     name     = module.traefik.app_name
     endpoint = "certificates"
@@ -45,12 +44,12 @@ resource "juju_integration" "traefik_certs" {
     name     = module.certificates.app_name
     endpoint = "certificates"
   }
+  model_uuid = juju_model.core.uuid
 }
 
 // openfga
 
 resource "juju_integration" "openfga_db" {
-  model = juju_model.core.name
   count = var.enable_admin_ui ? 1 : 0
 
   application {
@@ -59,7 +58,8 @@ resource "juju_integration" "openfga_db" {
   }
 
   application {
-    name     = juju_application.openfga[0].name
+    name     = module.openfga.app_name
     endpoint = "database"
   }
+  model_uuid = juju_model.core.uuid
 }
