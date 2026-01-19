43d575e11cdcab855e2e48d5fca1f32e1c0f504b
shipperizer <alexcabb@gmail.com>
2026-01-19 17:05:40 +0000

    docs: add example deployment with hook-service, providing custom claims capabilities

---
+++ examples/custom-claims/integrations.tf
@@ -0,0 +1,94 @@
+resource "juju_offer" "traefik_route" {
+  name             = "traefik-route"
+  application_name = module.traefik.app_name
+  endpoints        = ["traefik-route"]
+  model_uuid       = juju_model.core.uuid
+}
+
+resource "juju_offer" "postgresql" {
+  name             = "postgresql"
+  application_name = module.postgresql.application_name
+  endpoints        = ["database"]
+  model_uuid       = juju_model.core.uuid
+}
+
+resource "juju_offer" "send_ca_certificate" {
+  name             = "send-ca-cert"
+  application_name = module.certificates.app_name
+  endpoints        = ["send-ca-cert"]
+  model_uuid       = juju_model.core.uuid
+}
+
+resource "juju_offer" "openfga" {
+  name             = "openfga"
+  application_name = module.openfga.app_name
+  endpoints        = ["openfga"]
+  model_uuid       = juju_model.core.uuid
+}
+
+resource "juju_integration" "traefik_certs" {
+  application {
+    name     = module.traefik.app_name
+    endpoint = "certificates"
+  }
+
+  application {
+    name     = module.certificates.app_name
+    endpoint = "certificates"
+  }
+  model_uuid = juju_model.core.uuid
+}
+
+resource "juju_integration" "openfga_db" {
+
+  application {
+    name     = module.postgresql.application_name
+    endpoint = "database"
+  }
+
+  application {
+    name     = module.openfga.app_name
+    endpoint = "database"
+  }
+  model_uuid = juju_model.core.uuid
+}
+
+resource "juju_integration" "hydra_hook_service_token_hook" {
+  model_uuid = juju_model.iam.uuid
+
+  application {
+    name     = "hydra"
+    endpoint = "hydra-token-hook"
+  }
+
+  application {
+    name     = module.hook_service.app_name
+    endpoint = "hydra-token-hook"
+  }
+}
+
+resource "juju_integration" "hook_service_database" {
+  model_uuid = juju_model.iam.uuid
+
+  application {
+    offer_url = juju_offer.postgresql.url
+  }
+
+  application {
+    name     = module.hook_service.app_name
+    endpoint = "pg-database"
+  }
+}
+
+resource "juju_integration" "hook_service_openfga" {
+  model_uuid = juju_model.iam.uuid
+
+  application {
+    offer_url = juju_offer.openfga.url
+  }
+
+  application {
+    name     = module.hook_service.app_name
+    endpoint = "openfga"
+  }
+}
---
+++ examples/custom-claims/providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    juju = {
+      source  = "juju/juju"
+      version = "~> 1.0.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "juju" {}
