cbb87c42531a102affe478b8bbdc95bd4d17e005
shipperizer <alexcabb@gmail.com>
2026-08-17 20:57:36 +0200

    fixup! 7a93f59

--- examples/istio/main.tf
+++ examples/istio/main.tf
@@ -60,19 +60,6 @@ module "postgresql" {
   depends_on = [juju_model.core]
 }
 
-module "openfga" {
-  source = "github.com/canonical/openfga-operator//terraform?ref=v1.6.2"
-
-  model    = juju_model.core.uuid
-  app_name = "openfga-k8s"
-
-  config  = var.openfga.config
-  units   = var.openfga.units
-  channel = var.openfga.channel
-
-  depends_on = [juju_model.core, module.postgresql]
-}
-
 resource "juju_model" "iam" {
   name = "iam"
 }
@@ -91,35 +78,4 @@ module "iam" {
   enable_kratos_external_idp_integrator = var.enable_kratos_external_idp_integrator
 
   depends_on = [juju_model.iam]
-}
-
-resource "juju_secret" "hook_service_salesforce_credentials" {
-  name = "hook_service_salesforce_credentials"
-  value = {
-    consumer-key    = var.hook_service_consumer_key
-    consumer-secret = var.hook_service_consumer_secret
-  }
-  info       = "The credentials for calling the Salesforce API"
-  model_uuid = juju_model.iam.uuid
-}
-
-resource "juju_access_secret" "hook_service_credentials_access" {
-  applications = [var.hook_service.name]
-  secret_id    = juju_secret.hook_service_salesforce_credentials.secret_id
-  model_uuid   = juju_model.iam.uuid
-}
-
-module "hook_service" {
-  source = "git::https://github.com/canonical/hook-service-operator//terraform?ref=v1.0.6"
-
-  model = juju_model.iam.uuid
-
-  app_name                         = var.hook_service.name
-  channel                          = var.hook_service.channel
-  base                             = var.hook_service.base
-  revision                         = var.hook_service.revision
-  salesforce_credentials_secret_id = juju_secret.hook_service_salesforce_credentials.secret_id
-  config                           = var.hook_service.config
-
-  depends_on = [juju_access_secret.hook_service_credentials_access, module.iam]
 }
